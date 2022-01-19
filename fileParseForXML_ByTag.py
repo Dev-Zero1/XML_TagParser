fe65eca2b5280ff90636c3734752e9ce89ca0255
Dev-Zero1 <36249579+Dev-Zero1@users.noreply.github.com>
2022-01-18 19:49:09 -0800

    v1
    v1 initial commit

---
+++ fileParseForXML_ByTag.py
@@ -0,0 +1,32 @@
+import os
+from xml.dom import minidom
+import time
+
+path = "C://python//scripts//data//xml//"
+path2 = "C://python//scripts//compiledData//"
+fileType = ".xml"
+os.remove((path2+'data.txt'))
+time.sleep(2)
+##dir_list = os.listdir(path)
+
+writeFile = open((path2+'data.txt'),"a")
+processed = 0
+
+##print("Files and directories in '",path,"' :")
+
+for filename in os.listdir(path):
+    if not filename.endswith(fileType): continue
+    fullname = os.path.join(path, filename)
+    print(filename)
+    print("-----------------------------")
+    print("-----------------------------")
+    file = minidom.parse(fullname)
+    models = file.getElementsByTagName('map')
+    for elem in models:
+        print(elem.firstChild.data)
+        writeFile.write(elem.firstChild.data + ",/n")
+        processed+=1
+print("-----------------------------")        
+print(str(processed) + ' Files processed')        
+writeFile.close()
+    
